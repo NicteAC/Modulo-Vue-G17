9d78f30e79eac3cd8973284b16ef3b8405251b64
Carola <carola.aliaga@gmail.com>
2022-04-06 22:01:16 -0400

    Update 06-04-2022
    Co-authored-by: Ambar Medina <4WM3DINA@users.noreply.github.com>
    Co-authored-by: Carlos Araya Salfate <Charlie2208@users.noreply.github.com>
    Co-authored-by: Florangel Orochena <florangel26@users.noreply.github.com>
    Co-authored-by: ElThea07 <ElThea07@users.noreply.github.com>
    Co-authored-by: Naldi Castro Serrano <NeaCS@users.noreply.github.com>

---
+++ components/src/components/NuevoComponente.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <p class="saludo">Hola este es un nuevo componente</p>
+      <p>{{msg2}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "NuevoComponente",
+    props: {
+        msg2: {
+            type: String, 
+        },
+    },
+}
+</script>
+
+<style>
+.saludo {
+    font-size: 40px;
+}
+
+</style>
---
+++ directivas-y-eventos/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h1>Crear nueva tarea</h1>
+    <label>
+      Tarea
+      <input
+        type="text"
+        v-model="tarea"
+        placeholder="Ingrese una nueva tarea"
+      />
+    </label>
+    <button type="button" class="btn btn-primary mx-2" @click="addTarea">
+      Crear
+    </button>
+
+    <ul v-for="(tarea, index) in tareas" :key="index">
+      <li>
+        <strong> {{ index }}: {{ tarea }} </strong>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      tarea: "",
+      tareas: [],
+    };
+  },
+  methods: {
+    addTarea() {
+      const { tarea } = this;
+      this.tareas.push(tarea);
+      this.tarea = "";
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #333;
+  background-color: pink;
+  margin-top: 60px;
+}
+</style>
